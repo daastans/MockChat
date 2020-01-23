5597f18c3fa56868aa504742087efea7ab35d806
Aman Sharma <amansharma.pcmip@outlook.com>
2020-01-23 15:16:15 +0530

    Implementing photopicker using firebase cloud storage

--- app/src/main/java/com/google/firebase/mock/chat/MainActivity.java
+++ app/src/main/java/com/google/firebase/mock/chat/MainActivity.java
@@ -18,6 +18,7 @@ package com.google.firebase.mock.chat;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.icu.util.Freezable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -38,6 +39,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -45,7 +47,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -59,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
     private static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER =  2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -74,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener childEventListener;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotoReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+
+        mFirebaseStorage=FirebaseStorage.getInstance();
+        mChatPhotoReference=mFirebaseStorage.getReference().child("chat_photos");
 
 
         // Initialize references to views
@@ -107,7 +119,10 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(intent,"complete action using "),RC_PHOTO_PICKER);
             }
         });
 
@@ -183,6 +198,19 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+            else if(requestCode==RC_PHOTO_PICKER && resultCode==RESULT_OK){
+                Uri selectedImageUri=data.getData();
+                StorageReference photoRef=mChatPhotoReference.child(selectedImageUri.getLastPathSegment());
+                photoRef.putFile(selectedImageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        String downloadUrl=taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                        FriendlyMessage friendlyMessage=new FriendlyMessage(null,mUsername,downloadUrl);
+                        mDatabaseReference.push().setValue(friendlyMessage);
+                    }
+                });
+        }
+
     }
     @Override
     public void onPause(){
